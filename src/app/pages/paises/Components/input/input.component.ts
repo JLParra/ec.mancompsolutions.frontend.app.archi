1d96609c0a712b64c5cbf9656131c613d8754852
desktop-weir <soporte@weir.local>
2022-02-14 20:22:46 -0500

    configuracion del debounce placeholder general

--- src/app/pages/paises/Components/input/input.component.ts
+++ src/app/pages/paises/Components/input/input.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 ;
 
 @Component({
@@ -7,14 +9,34 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styles: [
   ]
 })
-export class InputComponent {
+export class InputComponent implements OnInit {
 
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
-   termino: string = "";
-  constructor() { }
+  @Output() onDebunce: EventEmitter<string> = new EventEmitter();
+  @Input() placeholder:string="";
+  debouncer: Subject<string> = new Subject();
+
+  termino: string = "";
+
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe(valor => {
+        this.onDebunce.emit(valor);
+      });
+  }
 
   buscar() {
     this.onEnter.emit(this.termino);
+  }
+  teclaPresionada() {
+    this.debouncer.next(this.termino);
+  }
+  sugerencias(){
+    
+    //TODO: CREAR SUGERENCIAS
+
   }
 
 }
